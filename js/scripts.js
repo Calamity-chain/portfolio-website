be4f68ea98e947e4aedaa192690f1fbfb18c8ccb
Calamity-chain <78163718+Calamity-chain@users.noreply.github.com>
2021-07-09 14:57:05 +0200

    Added a Jquery image viewer to work page

--- js/scripts.js
+++ js/scripts.js
@@ -53,3 +53,19 @@ if (validateForm()) {
 emailInput.addEventListener('input', validateEmail);
 passwordInput.addEventListener('input', validatePassword);
 })();
+
+//IMAGE VIEWER FUNCTION
+var $image = $('#image');
+
+$image.viewer({
+  inline: true,
+  viewed: function() {
+    $image.viewer('zoomTo', 1);
+  }
+});
+
+// Get the Viewer.js instance after initialized
+var viewer = $image.data('viewer');
+
+// View a list of images
+$('#images').viewer();
